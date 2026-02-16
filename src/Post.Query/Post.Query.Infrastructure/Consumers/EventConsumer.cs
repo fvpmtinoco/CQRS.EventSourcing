77a565e200494bb028864e42cb4026ffaf2d75d1
fernando.tinoco <fernando.tinoco@neuraspace.com>
2026-02-16 18:55:49 +0000

    Event consumer implementation

---
+++ src/Post.Query/Post.Query.Infrastructure/Consumers/EventConsumer.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using CQRS.Core.Consumers;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using Post.Query.Infrastructure.Converters;
+using Post.Query.Infrastructure.Handlers;
+using System.Text.Json;
+
+namespace Post.Query.Infrastructure.Consumers
+{
+    public class EventConsumer(IOptions<ConsumerConfig> config, IEventHandler eventHandler) : IEventConsumer
+    {
+        private readonly ConsumerConfig _config = config.Value;
+        private readonly IEventHandler _eventHandler = eventHandler;
+
+
+        public void Consume(string topic)
+        {
+            using var consumer = new ConsumerBuilder<string, string>(_config)
+                                        .SetKeyDeserializer(Deserializers.Utf8)
+                                        .SetValueDeserializer(Deserializers.Utf8)
+                                        .Build();
+
+            consumer.Subscribe(topic);
+
+            var options = new JsonSerializerOptions
+            {
+                Converters = { new EventJsonConverter() }
+            };
+
+            while (true)
+            {
+                var consumerResult = consumer.Consume();
+
+                if (consumerResult?.Message == null)
+                    continue;
+
+                var @event = JsonSerializer.Deserialize<BaseEvent>(consumerResult.Message.Value, options);
+                var handlerMethod = _eventHandler.GetType().GetMethod("On", [@event!.GetType()]);
+
+                if (handlerMethod != null)
+                {
+                    throw new ArgumentNullException(nameof(handlerMethod), "Could not fin event handler method");
+                }
+                handlerMethod!.Invoke(_eventHandler, [@event]);
+                consumer.Commit(consumerResult);
+            }
+        }
+    }
+}
